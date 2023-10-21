eebec50f48db8b1b124d161e8f642ed7de1cc9d8
frs11~ <fayezurrahman017@gmail.com>
2023-10-21 23:24:58 +0600

    final commit after fixing bug

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-// app.use(cors());
-const corsConfig = {
-  origin: "",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
-app.use(cors(corsConfig));
-app.options("", cors(corsConfig));
+app.use(cors());
+// const corsConfig = {
+//   origin: "",
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
+// app.use(cors(corsConfig));
+// app.options("", cors(corsConfig));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jmuxmrg.mongodb.net/?retryWrites=true&w=majority`;
@@ -113,11 +113,11 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.get("/", (req, res) => {
-  res.send("server is running");
-});
-app.get("/test", (req, res) => {
-  res.send("server testing");
-});
+// app.get("/", (req, res) => {
+//   res.send("server is running");
+// });
+// app.get("/test", (req, res) => {
+//   res.send("server testing");
+// });
 
 app.listen(port, () => console.log(`server is running on port: ${port}`));
